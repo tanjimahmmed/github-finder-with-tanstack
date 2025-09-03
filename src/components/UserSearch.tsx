cfaa1b8934a5fd96045d343e0ff84a2c88c8f3cb
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-03 12:14:59 +0600

    add rcentUsers saved in localstorage

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { fetchGithubUser } from "../api/github";
 import { UserCard } from "./UserCard";
 import RecentSearches from "./RecentSearches";
@@ -7,7 +7,10 @@ import RecentSearches from "./RecentSearches";
 const UserSearch = () => {
     const [username, setUsername] = useState('');
     const [submittedUsername, setSubmittedUsername] = useState('');
-    const [recentUsers, setRecentUsers] = useState<string[]>([]);
+    const [recentUsers, setRecentUsers] = useState<string[]>(() => {
+        const stored = localStorage.getItem('recentUsers');
+        return stored ? JSON.parse(stored) : [];
+    });
 
     const {data, isLoading, isError, error} = useQuery({
         queryKey: ['users', submittedUsername],
@@ -26,6 +29,8 @@ const UserSearch = () => {
             return updated.slice(0, 5);
         });
     }
+
+    useEffect(() => {localStorage.setItem('recentUsers', JSON.stringify(recentUsers))}, [recentUsers])
 
   return (
     <>
