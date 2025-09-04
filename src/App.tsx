00e1a246bc0217e18ea720254038502447eeb483
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2025-09-04 09:57:32 +0600

    add toast for notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import UserSearch from "./components/UserSearch"
+import UserSearch from "./components/UserSearch";
+import { Toaster } from "sonner";
 
 const App = () => {
   return (
     <div className="container">
       <h1>Github Finder</h1>
       <UserSearch/>
+      <Toaster/>
     </div>
   )
 }
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,4 +1,5 @@
 import { useQuery, useMutation } from "@tanstack/react-query";
+import {toast} from 'sonner';
 import { checkIfFollowingUser, followGithubUser, unfollowGithubUser } from "../api/github";
 import { FaGithubAlt, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import type { GithubUser } from "../types";
@@ -15,11 +16,11 @@ export const UserCard = ({user}: {user: GithubUser}) => {
     const followMutation = useMutation({
         mutationFn: () => followGithubUser(user.login),
         onSuccess: () => {
-            console.log(`You are now following ${user.login}`);
+            toast.success(`You are now following ${user.login}`);
             refetch();
         },
         onError: (err) => {
-            console.log(err.message)
+            toast.error(err.message)
         }
     });
 
@@ -27,11 +28,11 @@ export const UserCard = ({user}: {user: GithubUser}) => {
     const unfollowMutation = useMutation({
         mutationFn: () => unfollowGithubUser(user.login),
         onSuccess: () => {
-            console.log(`You are no longer following ${user.login}`);
+            toast.success(`You are no longer following ${user.login}`);
             refetch();
         },
         onError: (err) => {
-            console.log(err.message)
+            toast.error(err.message)
         }
     });
 
